bcfa1224fa708610f7dabea5029d7fb1608a18d1
Alona <alenarizhova@gmail.com>
2020-03-31 22:55:09 +0300

    dfghjklkjhgtfds

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,9 +12,9 @@ public class Main {
         Manager manager2 = new Manager(20002, "Viktor", "Kravchuk", 23000, manager1,400);
         Manager manager3 = null;
         manager3.topManager(20003, "Oleg", "Box", 30000, 5000);
-        Manager manager4 = new Manager(20004,"Vlad","Lisuy", 2500000, manager3,4000);
-        Manager manager5 = new Manager(20005, "Vadim", "Bylochka", 20000, manager4, 3000);
-        Manager manager6 = new Manager(20006, "Volodya","Pirigok", 19000, manager5,2000);
+        Manager manager4 = new Manager(4844,"Vlad","Lisuy", 2500000, manager3,4000);
+        Manager manager5 = new Manager(52484, "Vadim", "Bylochka", 20000, manager4, 3000);
+        Manager manager6 = new Manager(555959, "Volodya","Pirigok", 19000, manager5,2000);
 
         Employees employee1 = new Employees(10001, "Max", "Manko", 15000, manager2 );
         Employees employee2 = new Employees(10002, "Denys", "Krok", 15000, manager2);
